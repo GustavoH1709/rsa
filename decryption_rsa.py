19558f49f1442df29eb774488cb16e6b7bee5586
GustavoH1709 <gustavoh1709@hotmail.com>
2023-07-02 20:42:58 -0300

    a

--- decryption_rsa.py
+++ decryption_rsa.py
@@ -1,27 +1,53 @@
 import os
+import base64
 
 #chave publica 4653899
 #chave privada 2255579
+#expoente 5993839
 
-private_key = 2255579
-n = 2027 * 2957
+#n = 2027 * 2957
 
 def check_valid_path(path):
     return os.path.isfile(path)
 
-output_file_path = str(input('Insira o caminho do arquivo de saída: '))
+def base64_to_string(base64_string):
+    base64_bytes = base64_string.encode('ascii')
+    string_bytes = base64.b64decode(base64_bytes)
+    original_string = string_bytes.decode('ascii')
+    return original_string
 
-if(check_valid_path(output_file_path) is False):
-    raise SystemError('Caminho para arquivo de saida é inválido')
+private_key_and_power_file_path = str(input("Insira o caminho do arquivo com a chave privada e o expoente: "))
+
+if(check_valid_path(private_key_and_power_file_path) is False):
+    raise SystemError('Caminho para arquivo com as chave privada é inválido')
+
+encoded_message_file_path = str(input('Insira o caminho do arquivo de com a mensagem criptografada: '))
+
+if(check_valid_path(encoded_message_file_path) is False):
+    raise SystemError('Caminho para arquivo com a mensagem criptografada é inválido')
+
+decoded_output_file_path = str(input("Insira o caminho do arquivo de saída"))
+
+private_key = 0;
+power = 0;
+
+with open(private_key_and_power_file_path, 'r') as file:
+    keys_lines = file.readlines()
+    private_key = int(keys_lines[0]);
+    power = int(keys_lines[1]);
 
 output_file_lines = []
 
-with open(output_file_path, 'r') as file:
+with open(encoded_message_file_path, 'r') as file:
     for item in file:
         if(item != '\n' and item != ''):
-            output_file_lines.append(int(item.replace('\n', '')))
+            output_file_lines.append(int(item.replace('\n', '').replace(' ', '')))
+    file.close()
 
-message_encoded = [pow(c, private_key, n) for c in output_file_lines]
+message_encoded = [pow(c, private_key, power) for c in output_file_lines]
 message_translate = "".join(chr(ch) for ch in message_encoded)
+message_64_to_string = base64_to_string(message_translate)
 
-print(message_translate)
+with open(decoded_output_file_path, 'w') as file:
+    file.write(message_64_to_string)
+    file.close();
--- encryption_rsa.py
+++ encryption_rsa.py
@@ -1,20 +1,28 @@
 import os
+import base64
 
 #chave publica 4653899
 #chave privada 2255579
+#numeros primos 2027 e 2957
+#expoente 5993839
 
 def check_valid_path(path):
     return os.path.isfile(path)
 
-private_key = 2255579
+def string_to_base64(string):
+    string_bytes = string.encode('ascii')
+    base64_bytes = base64.b64encode(string_bytes)
+    base64_string = base64_bytes.decode('ascii')
+    return base64_string
+
 n = 2027 * 2957
 
-file_path_public_key = str(input("Insira o caminho da pasta com a chave pública: "))
+file_path_public_key = str(input("Insira o caminho da pasta com a chave pública e privada: "))
 
 if(check_valid_path(file_path_public_key) is False):
     raise SystemError('Caminho para arquivo com a chave pública é inválido')
 
-file_input_path = str(input("Insira o caminho do arquivo de entrada: "))
+file_input_path = str(input("Insira o caminho do arquivo do texto que vai ser criptografado: "))
 
 if(check_valid_path(file_input_path) is False):
     raise SystemError('Caminho para arquivo de entrada é inválido')
@@ -26,22 +34,20 @@ message = '';
 with open(file_input_path, 'r') as file:
     message = file.read().replace('\n', '')
 
-public_key = '';
+message_to_64 = string_to_base64(message);
 
 with open(file_path_public_key, 'r') as file:
-    public_key = int(file.read().replace('\n', ''))
+    input_encryption_lines = file.readlines()
 
-print(public_key)
+public_key = int(input_encryption_lines[0]);
+private_key = int(input_encryption_lines[1]);
 
-message_encoded = [ord(c) for c in message]
+message_encoded = [ord(c) for c in message_to_64]
 ciphertext = [pow(c, public_key, n) for c in message_encoded]
 
 f = open(file_output_path, 'w')
 f.writelines(str(line) + '\n' for line in ciphertext)
 f.close()
 
-
 message_encoded = [pow(c, private_key, n) for c in ciphertext]
-
-print("".join(chr(ch) for ch in message_encoded))
 
